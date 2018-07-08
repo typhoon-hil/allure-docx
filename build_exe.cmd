a2f397a2304cffa39b55e35b6d721173b95aca94
victor <victor.maryama@typhoon-hil.com>
2018-07-08 15:54:02 +0200

    Embedded cairo.dll

--- build_exe.cmd
+++ build_exe.cmd
@@ -1,1 +1,1 @@
-pyinstaller -F --clean --add-data=src\allure_docx\template.docx;. --additional-hooks-dir=. --log-level=DEBUG -n allure-docx src\allure_docx\commandline.py
+pyinstaller -F --clean --add-data=libs\cairo.dll;cairo --add-data=src\allure_docx\template.docx;. --additional-hooks-dir=. --log-level=DEBUG -n allure-docx src\allure_docx\commandline.py
