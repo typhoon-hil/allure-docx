d48dc0be57a8c62635722551320fec9e8c59530e
Bjarne Scheimann <bjarne.scheimann@ise.fraunhofer.de>
2022-10-05 19:05:36 +0200

    updated version and CHANGELOG.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="allure-docx",
     description="docx report generator based on allure-generated json files",
     author="Victor Maryama (Typhoon HIL, Inc)",
-    version="0.3.2",
+    version="0.4.0",
     license="MIT",
     install_requires=[
         'cairosvg',
